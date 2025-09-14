17bc06698181a994d558ce2cd536d9b3bb0f2a6a
cheyang <cheyang@163.com>
2025-09-14 16:40:30 +0800

    feat: Add support for 1:1 rbg per topology assignment
    Signed-off-by: cheyang <cheyang@163.com>

--- api/workloads/v1alpha1/helper.go
+++ api/workloads/v1alpha1/helper.go
@@ -72,14 +72,8 @@ func (rbg *RoleBasedGroup) GetExclusiveKey() (topologyKey string, found bool) {
 	return
 }
 
-func (rbg *RoleBasedGroup) GenGroupUniqueKey() (key string) {
-	key = rbg.Labels[SetGroupUniqueHashLabelKey]
-	if key != "" {
-		return key
-	} else {
-		key = sha1Hash(fmt.Sprintf("%s/%s", rbg.GetNamespace(), rbg.GetName()))
-	}
-	return
+func (rbg *RoleBasedGroup) GenGroupUniqueKey() string {
+	return sha1Hash(fmt.Sprintf("%s/%s", rbg.GetNamespace(), rbg.GetName()))
 }
 
 // sha1Hash accepts an input string and returns the 40 character SHA1 hash digest of the input string.
